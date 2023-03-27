21b9cd185c44bd0747d91a5cd5088774e263a123
shivanisolanki <shivanisolanki9691@gmail.com>
2023-03-27 14:37:50 +0530

    change in database

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,4 +90,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { :host => "dpg-cge25sqk728lv80g24ug-a"}
+
+  config.active_record.dump_schema_after_migration = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => "587",
+    :domain => "gmail.com",
+    :user_name => "shivakewat2099@gmail.com",
+    :password => "rzrbvzhsfbjihbth",
+    :authentication => "plain",
+    :enable_starttls_auto => true
+  } 
 end
